4f3f1cba474be3e27088e2650e4bf4a85cc4d7e1
Piotr Januszewski <piotr.januszewski@quantumlab.co>
2018-04-16 16:51:30 +0200

    Add data and weights visualization

--- keras_demo.py
+++ keras_demo.py
@@ -5,6 +5,9 @@ from keras.utils import to_categorical
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 # Prepare data
 digits = datasets.load_digits()
 # NOTE: Explain what is stratified division and why we should use it
@@ -12,6 +15,13 @@ data_train, data_test, target_train, target_test = \
   train_test_split(digits.data, digits.target, test_size=0.2, random_state=7, stratify=digits.target)
 _, n_features = digits.data.shape
 
+# Visualize data
+IMAGE_SHAPE = (8, 8)
+for i, image in enumerate(data_train[:20, :]):
+    plt.subplot(5, 4, i + 1)
+    plt.imshow(image.reshape(IMAGE_SHAPE), cmap=plt.cm.gray)
+plt.show()
+    
 # Prepare targets
 onehot_train = to_categorical(target_train) # NOTE: Explain what is onehot vector...
 onehot_test = to_categorical(target_test)   #       ...and why we use it.
@@ -30,7 +40,15 @@ model.compile( # NOTE: Show compile docs and explain what we use
 
 # Fit model
 # NOTE: Show fit docs and explain what we use and what it does
-model.fit(data_train, onehot_train, epochs=25, batch_size=24, validation_split=0.2)
+model.fit(data_train, onehot_train, epochs=25, batch_size=64, validation_split=0.1)
+
+# Visualize weights
+# NOTE: You can't see anything, images are too small
+layer = np.transpose(model.get_weights()[0])
+for i, image in enumerate(layer[:20, :]):
+    plt.subplot(5, 4, i + 1)
+    plt.imshow(image.reshape(IMAGE_SHAPE), cmap=plt.cm.gray)
+plt.show()
 
 # Evaluate model
 # NOTE: Show evaluate docs and explain what we use and what it does
